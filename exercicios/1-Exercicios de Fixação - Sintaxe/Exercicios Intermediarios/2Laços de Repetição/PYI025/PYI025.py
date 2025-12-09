2a97b35274ab5e22b35e56859bba6b4e4347623c
Filipe César <filipecesarufpe@gmail.com>
2025-12-09 19:40:12 -0300

    update

--- exercicios/1-Exercicios de Fixação - Sintaxe/Exercicios Intermediarios/2Laços de Repetição/PYI025/PYI025.py
+++ exercicios/1-Exercicios de Fixação - Sintaxe/Exercicios Intermediarios/2Laços de Repetição/PYI025/PYI025.py
@@ -1,14 +1,11 @@
-num = int(input('Entre com um número: '))
+soma = 0
 cont = 0
-while num > 0:
-    if num > 0:
-        num = int(input('Entre com um número: '))
+while True:
+    num = int(input('Entre com um número: '))
+    if num < 0:
+        break
+    else:
         soma +=num
         cont+=1
         media = soma/cont
-    else:
-        break
 print(f'A média dos numeros positivos digitados foi {media}')
-    
-
-
