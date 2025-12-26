d1807b96f043d3d2fafa87bde99b4629ea8cb9d6
Filipe <filipecesar2019@gmail.com>
2025-12-26 20:08:32 -0300

    update

--- exercicios/Projetos/Projeto Biblioteca/projetoBiblioteca.py
+++ exercicios/Projetos/Projeto Biblioteca/projetoBiblioteca.py
@@ -32,13 +32,50 @@ class Livro(Midia):
     def __init__(self, titulo, autor, ano, id):
         super().__init__(titulo, autor, ano, id)
     
-
+    def cadastrar(self):
+        pass
+    def listar(self):
+        pass
+    def buscar(self):
+        pass
+    def emprestar(self):        
+        Midia.set_dispor(transacao = 'emprestar')
+    def devolver(self):
+        Midia.set_dispor(transacao = 'devolver')
+    def remover(self):
+        pass
+    def get_dispor(self):
+        return self.__disponivel
+    def set_dispor(self, transacao):
+        if transacao == 'emprestar':
+            self.__disponivel = False
+        elif transacao == 'devolver':
+            self.__disponivel = True
 
 class Revista(Midia):
     def __init__(self, titulo, autor, ano, id):
         super().__init__(titulo, autor, ano, id)
 
-
+    def cadastrar(self):
+        pass
+    def listar(self):
+        pass
+    def buscar(self):
+        pass
+    def emprestar(self):        
+        Midia.set_dispor(transacao = 'emprestar')
+    def devolver(self):
+        Midia.set_dispor(transacao = 'devolver')
+    def remover(self):
+        pass
+    def get_dispor(self):
+        return self.__disponivel
+    def set_dispor(self, transacao):
+        if transacao == 'emprestar':
+            self.__disponivel = False
+        elif transacao == 'devolver':
+            self.__disponivel = True
+            
 class Filme(Midia):
     def __init__(self, titulo, autor, ano, id, isbn, edicao, duracao):
         super().__init__(titulo, autor, ano, id)
@@ -46,3 +83,22 @@ class Filme(Midia):
         self.edicao = edicao
         self.duracao = duracao
 
+    def cadastrar(self):
+        pass
+    def listar(self):
+        pass
+    def buscar(self):
+        pass
+    def emprestar(self):        
+        Midia.set_dispor(transacao = 'emprestar')
+    def devolver(self):
+        Midia.set_dispor(transacao = 'devolver')
+    def remover(self):
+        pass
+    def get_dispor(self):
+        return self.__disponivel
+    def set_dispor(self, transacao):
+        if transacao == 'emprestar':
+            self.__disponivel = False
+        elif transacao == 'devolver':
+            self.__disponivel = True
